926571f0e97dc1f04c31eba6a90185bfce077b36
YMarroquin.multilab <yurimarroquin.multilab@gmail.com>
2023-07-01 13:04:34 -0500

    agregar funcionalidad de formulario

--- js/index.js
+++ js/index.js
@@ -30,11 +30,12 @@ navbarItems.forEach(function(item) {
     });
   });
 
+  /** nabvar menu */
   function toggleMenu() {
     let navbarMenu = document.getElementById("navbarMenu");
     navbarMenu.classList.toggle("open");
   }
-  
+
   document.addEventListener('click', function(event) {
     var navbarCollapse = document.querySelector('.navbar-collapse');
     var navbarToggle = document.querySelector('.navbar-toggle');
@@ -44,6 +45,8 @@ navbarItems.forEach(function(item) {
     }
   });
 
+
+/**button scroll up */
 window.addEventListener('scroll', function() {
   let scrollButton = document.querySelector('.scroll-up');
   if (window.scrollY > 300) {
@@ -80,3 +83,64 @@ toggleDarkModeButton.addEventListener('change', function() {
     switchText.innerHTML = 'Modo dark';
   }
 });
+
+/**validacion de formulario */
+
+document.addEventListener('DOMContentLoaded', function() {
+  let form = document.getElementById('contact-form');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault(); // Evitar que el formulario se envíe de forma predeterminada
+
+    let minLength = 2;
+    let maxLength = 30;
+    let maxLengthMessage = 250;
+    let correoRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    let specialCharsRegex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+
+    // Aquí puedes acceder a los elementos del formulario y realizar las acciones necesarias, como validaciones o envío de datos al servidor
+
+    // Ejemplo de acceso a los elementos del formulario
+    let nameInput = form.elements['name'];
+    let emailInput = form.elements['email'];
+    let messageInput = form.elements['message'];
+    let name = nameInput.value;
+    let email = emailInput.value;
+    let message = messageInput.value;
+    email = email.trim();
+
+    /**validaciones */
+    if(name == "" || email == "" || message == ""){
+      mostrarError("Todos los campos son requeridos.");
+      return false;
+    }
+    if(name < minLength || name >maxLength){
+      mostrarError("El nombre debe tener minimo 2 y maximo 30 caracteres.");
+      return false;
+    }else if(specialCharsRegex.test(name)){
+      mostrarError("El nombre no debe contener caracteres especiales.");
+      return false;
+    }
+
+    if (!correoRegex.test(email)) {
+      mostrarError('El correo electrónico no es válido.');
+      return false;
+    }
+
+    if(message > maxLengthMessage){
+      mostrarError('La descripción debe ser menor a 250 caracteres');
+      return false;
+    }
+
+    emailjs.sendForm('service_v7v56yb', 'template_e8hahyd', "#contact-form", '4SEvjyy1v7aJnSu6T')
+    .then(function(response) {
+      swal("Correcto!", "El correo se envio correctamente", "success");
+    }, function(error) {
+       console.log('FAILED...', error);
+    });
+  });
+});
+
+function mostrarError(mensaje) {
+  swal("¡Atención!", mensaje, "info");
+}
